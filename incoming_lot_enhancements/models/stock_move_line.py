0d4c141ccf0c4ea835115a4280beaadef2fcff19
siddharth <siddharthj36@gmail.com>
2021-03-25 18:16:06 +0530

    [IMP] auto propogate destination location

--- incoming_lot_enhancements/models/stock_move_line.py
+++ incoming_lot_enhancements/models/stock_move_line.py
@@ -10,6 +10,12 @@ class StockMoveLine(models.Model):
     def default_get(self, fields):
         rec = super(StockMoveLine, self).default_get(fields)
         if self._context.get('add_lot_number_name', False):
+            picking = self.env['stock.picking'].browse(self._context['add_lot_number_name'])
+            for line in picking.move_ids_without_package:
+                if line.product_id.type == 'product':
+                    rec['product_id'] = line.product_id.id
+                    rec['location_dest_id'] = line.location_dest_id.id
+                    break
             lot_number = self.env.ref('incoming_lot_enhancements.sequence_operation_product').next_by_id()
             rec['lot_name'] = lot_number
         return rec
--- incoming_lot_enhancements/models/stock_picking.py
+++ incoming_lot_enhancements/models/stock_picking.py
@@ -6,15 +6,6 @@ from odoo import fields, models, api
 class Picking(models.Model):
     _inherit = 'stock.picking'
 
-    operation_product_id = fields.Many2one('product.product', 'Operation Product', compute="_compute_product_operation")
-
-    @api.depends('move_ids_without_package.product_id')
-    def _compute_product_operation(self):
-        for line in self.move_ids_without_package:
-            if line.product_id.type == 'product':
-                self.operation_product_id = line.product_id.id
-                break
-
     @api.multi
     def action_confirm(self):
         rec = super(Picking, self).action_confirm()
